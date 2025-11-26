4ad0f4f2312686f5b45163a27ea1ff0e9f5697a9
SengQT <kyexd16@gmail.com>
2025-11-26 20:54:01 +0700

    Add Dockerfile and configure for deployment1

--- obj/Debug/net9.0/ClinicWEB.AssemblyInfo.cs
+++ obj/Debug/net9.0/ClinicWEB.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClinicWEB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a74c40d3bd3d9dbb8e5e900c44ed4d86d5b099c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95473171d0a994076b1644f85eee9c8ef935367b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClinicWEB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClinicWEB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
